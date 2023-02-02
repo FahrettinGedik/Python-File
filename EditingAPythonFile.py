b849466f395ac5aeac2cd7346ed89f09489507e2
Fahrettin Gedik <fahged41@gmail.com>
2023-02-02 20:09:45 +0100

    second commit

---
+++ EditingAPythonFile.py
@@ -0,0 +1,24 @@
+print("Python has three numeric types: int, float, and complex")
+myValue = 1
+print(myValue)
+print(type(myValue))
+
+myValueFloat = 3.14
+print(myValueFloat)
+print(str(myValueFloat) + " is of the data type " + str(type(myValueFloat)))
+
+myValueComplexDataType = 5j
+print(myValueComplexDataType)
+print(type(myValueComplexDataType))
+print(str(myValueComplexDataType) + " is of the data type " +
+      str(type(myValueComplexDataType)))
+
+myValueBool = True
+print(myValueBool)
+print(type(myValueBool))
+print(str(myValueBool) + " is the data type " + str(type(myValueBool)))
+
+myValueBoolF = False
+print(myValueBoolF)
+print(type(myValueBoolF))
+print(str(myValueBoolF) + " is the data type " + str(type(myValueBoolF)))
---
+++ stringType.py
@@ -0,0 +1,4 @@
+myString = "This is a string."
+print(myString)
+print(type(myString))
+print(myString + " is the data type " + str(type(myString)))
