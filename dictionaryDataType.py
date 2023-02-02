c40c0c87d01ad8db1d468ae84296a4d698e2f8bc
Fahrettin Gedik <fahged41@gmail.com>
2023-02-02 21:21:22 +0100

    fourth commit

---
+++ dictionaryDataType.py
@@ -0,0 +1,11 @@
+myFavoriteDictionary = {
+    "Akua": "apple",
+    "Saanvi": "banana",
+    "Paulo": "pineapple"
+}
+print(myFavoriteDictionary)
+print(type(myFavoriteDictionary))
+
+print(myFavoriteDictionary["Akua"])
+print(myFavoriteDictionary["Saanvi"])
+print(myFavoriteDictionary["Paulo"])
