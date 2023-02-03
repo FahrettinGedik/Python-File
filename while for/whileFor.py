71009fda4939114310c5c40d7ba7162a240dd5b6
Fahrettin Gedik <fahged41@gmail.com>
2023-02-03 23:47:17 +0100

    if elif While for

---
+++ while for/whileFor.py
@@ -0,0 +1,8 @@
+import random
+print("Welcome to Guess the Number!")
+print("The rules are simple. I will think of a number, and you will try to guess it.")
+random.random()
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+get = random.choice(nums)
+print(get)
